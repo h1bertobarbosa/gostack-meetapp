19334be2d98c7ad81670d33119f278ce41fbbe7d
humberto <humberto.obarbosa@gmail.com>
2019-08-15 22:27:32 -0300

    add navegacao por data no dashboard

--- mobile/src/pages/Dashboard/index.js
+++ mobile/src/pages/Dashboard/index.js
@@ -1,28 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { withNavigationFocus } from 'react-navigation';
-import { Alert } from 'react-native';
+import { Alert, TouchableOpacity, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '~/services/api';
 import { Container, Title, List } from './styles';
 import Background from '~/components/Background';
 import Meetapp from '~/components/Meetapp';
-import DateInput from '~/components/DateInput';
+import { addDays, subDays, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
 function Dashboard({ isFocused }) {
   const [date, setDate] = useState(new Date());
   const [meetapps, setMeetapps] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   async function loadMeetapps() {
     const response = await api.get('meetups', {
       params: {
         date: date.toISOString(),
       },
     });
+    setLoading(false);
     setMeetapps(response.data);
   }
 
   useEffect(() => {
     loadMeetapps();
   }, [date]);
+
+  const dateFormatted = useMemo(
+    () => format(date, "dd 'de' MMMM 'de' yyyy", { locale: pt }),
+    [date]
+  );
 
   async function handleSubscription(id) {
     try {
@@ -33,21 +42,46 @@ function Dashboard({ isFocused }) {
     }
   }
 
+  function handleSelectDate(side) {
+    setLoading(true);
+    if (side === 'left') {
+      setDate(subDays(date, 1));
+    } else {
+      setDate(addDays(date, 1));
+    }
+  }
+
   return (
     <Background>
       <Container>
-        <DateInput date={date} onChange={setDate} />
+        <TouchableOpacity
+          style={styles.chevronLeft}
+          onPress={() => handleSelectDate('left')}
+        >
+          <Icon name="chevron-left" size={20} color="#fff" />
+        </TouchableOpacity>
+        <Title>{dateFormatted}</Title>
+        <TouchableOpacity
+          style={styles.chevronRight}
+          onPress={() => handleSelectDate('right')}
+        >
+          <Icon name="chevron-right" size={20} color="#fff" />
+        </TouchableOpacity>
 
-        <List
-          data={meetapps}
-          keyExtractor={item => String(item.id)}
-          renderItem={({ item }) => (
-            <Meetapp
-              onSubscription={() => handleSubscription(item.id)}
-              data={item}
-            />
-          )}
-        />
+        {loading ? (
+          <Title>Carregando...</Title>
+        ) : (
+          <List
+            data={meetapps}
+            keyExtractor={item => String(item.id)}
+            renderItem={({ item }) => (
+              <Meetapp
+                onSubscription={() => handleSubscription(item.id)}
+                data={item}
+              />
+            )}
+          />
+        )}
       </Container>
     </Background>
   );
@@ -59,5 +93,26 @@ Dashboard.navigationOptions = {
     <Icon name="list" size={20} color={tintColor} />
   ),
 };
+
+const styles = StyleSheet.create({
+  chevronRight: {
+    marginTop: 33,
+    marginLeft: '80%',
+    backgroundColor: 'transparent',
+    color: 'rgba(255,255,255,1)',
+    position: 'absolute',
+    fontSize: 30,
+    fontFamily: 'material',
+  },
+  chevronLeft: {
+    marginTop: 33,
+    marginLeft: '15%',
+    backgroundColor: 'transparent',
+    color: 'rgba(255,255,255,1)',
+    position: 'absolute',
+    fontSize: 30,
+    fontFamily: 'material',
+  },
+});
 
 export default withNavigationFocus(Dashboard);
--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // 192.168.100.11 = iw
 // genymotion 10.0.3.2
 const api = axios.create({
-  baseURL: 'http://192.168.100.11:3333',
+  baseURL: 'http://192.168.0.104:3333',
 });
 
 export default api;
